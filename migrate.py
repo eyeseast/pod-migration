2e58fe60a8d20707b2fb349dcdacf2afb4984ee4
chrisamico <eyeseast@gmail.com>
2014-01-16 17:10:51 -0500

    Template logic. Each table gets an XML file.

--- migrate.py
+++ migrate.py
@@ -8,9 +8,38 @@ prepopulated with Pod data.
 """
 
 import dataset
+import os
+
 from decimal import Decimal
+from jinja2 import Environment, FileSystemLoader
 
 from db import db
 from schema import TABLES
 
+# template setup
+# relative path is ok since this won't move
+env = Environment(loader=FileSystemLoader('./templates'))
 
+def main():
+    """
+    For each table,
+    loop through rows
+    convert field names
+    render a template
+    write to output folder.
+    """
+    for name, schema in TABLES.iteritems():
+
+        if not schema:
+            continue
+        
+        template = env.get_template(schema['template'])
+        fields = schema['fields']
+        table = db.load_table(name) # fail if the name is wrong
+
+        with open('./output/%s.xml' % name, 'w') as output:
+            output.write(template.render().encode('utf-8'))
+
+
+if __name__ == '__main__':
+    main()
